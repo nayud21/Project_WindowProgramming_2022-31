948650dc04e71e49bbd5e2adf48f3876e5dadde9
Lam Hong Anh Duc <Lam Hong Anh Duc@LAPTOP-5LSCQBF5>
2024-11-11 22:28:36 +0700

    this rework is version 2 of last develop, other rework way

---
+++ SellingTree/Helper/ProductHandle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SellingTree.Model;
+using SellingTree.ViewModel;
+
+namespace SellingTree.Helper
+{
+    public class ProductHandle
+    {
+
+        public static Product findProductByName(String name)
+        {
+            Product ans = null;
+
+            MainViewViewModel productsViewModel = new MainViewViewModel();
+            for(int i = 0; i < productsViewModel.products.Count; i++)
+            {
+                if (productsViewModel.products[i].Name == name) { 
+                    ans = productsViewModel.products[i];
+                }
+            }
+
+            return ans;
+        }
+
+    }
+}
